f6713baeacc661772f088c51c32c6f7efbab6ebb
krish14-ai <kkhandelwal292@gmail.com>
2026-07-17 01:12:29 +0530

    still Studying Nested Models

--- pydantic/nested_model.py
+++ pydantic/nested_model.py
@@ -1,5 +1,18 @@
 from pydantic import BaseModel, Field, field_validator, model_validator,AnyUrl
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Annotated
 
+
+class Address(BaseModel):
+    city : str
+    state : str
+    pincode : str
+
+##--------------------------------------------------------------
 class Patient(BaseModel):
-    name : 
+    name : Annotated[str, Field(..., max_length = 100)]
+    age : Annotated[int,Field(..., ge = 0)]
+    gender : str
+    address: str
+    contact : List[str]
+
+##--------------------------------------------------------------
