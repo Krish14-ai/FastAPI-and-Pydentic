b4878e00a9b4916c31e784d58c21f964e6cb73c8
krish14-ai <kkhandelwal292@gmail.com>
2026-07-17 01:22:59 +0530

    still Studying

--- pydantic/nested_model.py
+++ pydantic/nested_model.py
@@ -12,7 +12,26 @@ class Patient(BaseModel):
     name : Annotated[str, Field(..., max_length = 100)]
     age : Annotated[int,Field(..., ge = 0)]
     gender : str
-    address: str
+    address: Address
     contact : List[str]
 
 ##--------------------------------------------------------------
+
+address_dict = {'city':'bareilly', 
+                'state': 'Uttar_Pradesh',
+                'pincode': '243005'
+                }
+
+address = Address(**address_dict)
+
+patient =  {
+    "name" : "Krish",
+    "age": 23, 
+    "gender": 'Male',
+    "address": address,
+    "contact" : ["123456789"], 
+}
+
+
+patient_1 = Patient(**patient)
+print(patient_1.address.city)
