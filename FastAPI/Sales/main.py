205cdca58df594e01740343505383fd623fdbf4c
krish14-ai <kkhandelwal292@gmail.com>
2026-09-07 00:46:06 +0530

    Done for today

--- FastAPI/Sales/main.py
+++ FastAPI/Sales/main.py
@@ -16,8 +16,14 @@ def get_everything():
 
 @app.get("/products/{id}")
 def get_product(id : int):
-    return {"messege" : Product.get_product(id)}
+    return {"message" : Product.get_product(id)}
 
-@app.get("/proucts")
-def list_products(name: str = Query(default = None, min_length = 1, max_length = 75, description = "Search product by name (case insensitive)", ) ):
-    return name 
+@app.get("/products")
+def list_products(name: str = Query( default = None, min_length = 1, max_length = 75, description  ="Search product by name (case insensitive)") ):
+
+    product = Product.get_all_products()
+    if name : 
+        needle = name.strip().lower()
+        Product = [p for p in product if needle in p.get("name","")]
+
+    return name
