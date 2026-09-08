3d3ec387cc4fe8bef8196568c7eca37178cc5b09
krish14-ai <kkhandelwal292@gmail.com>
2026-09-08 23:39:50 +0530

    Adding Order in products by price

--- FastAPI/Sales/main.py
+++ FastAPI/Sales/main.py
@@ -22,14 +22,24 @@ def get_product(id : int):
 
 @app.get("/products")
 def list_products(name: str = Query(default=None, min_length=1, max_length=75, description="Search product by name (case insensitive)"), 
-                  sort_by_price : bool = Query(default = "asc", description = "Sort products by price ") ):
+                  sort_by_price : bool = Query(default = False, description = "Sort products by price "),
+                  order : bool = Query(default = "asc", description = "Sort Order when sort_by_price = ture (asc,desc)") ):
     
     products = Product.get_all_products()
+
     if name:
         needle = name.strip().lower()
         products = [p for p in products if needle in p.get("name", "").lower()]
-        if not products:
-            raise HTTPException(status_code=404, detail=f"No product found named {name}")
+
+    if not products:
+        raise HTTPException(status_code=404, detail=f"No product found named {name}")
+
+    if sort_by_price : 
+        reverse = order == "desc"
+        
+    
+
 
     return products
+
 
