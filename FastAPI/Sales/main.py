208510c228c33553e70d496d87a4dda18d36b1b6
krish14-ai <kkhandelwal292@gmail.com>
2026-09-09 00:12:09 +0530

    .

--- FastAPI/Sales/main.py
+++ FastAPI/Sales/main.py
@@ -21,10 +21,11 @@ def get_product(id : int):
 
 
 @app.get("/products")
-def list_products(name: str = Query(default=None, min_length=1, max_length=75, description="Search product by name (case insensitive)"), 
-                  sort_by_price : bool = Query(default = False, description = "Sort products by price "),
-                  order : bool = Query(default = "asc", description = "Sort Order when sort_by_price = ture (asc,desc)"),
-                   limit : int = Query(default = 5,ge = 1, le =100 , description = "No Of items to return") ):
+def list_products(name: str = Query(default=None, min_length=1, max_length=75, description="Search product by name (case insensitive)"),## To get name of a Product
+                  sort_by_price : bool = Query(default = False, description = "Sort products by price "), ## True for sorting, False for no Sorting
+                  order : bool = Query(default = "asc", description = "Sort Order when sort_by_price = ture (asc,desc)"), ## Ascending or Descinding 
+                   limit : int = Query(default = 5,ge = 1, le =100 , description = "No Of items to return"), ## Total no of Items
+                 offset : int = Query(default= 0, ge =0 , le = 100, description = "Pagination offset") ): ## No of Pages to have
     
     products = Product.get_all_products()
 
@@ -39,8 +40,9 @@ def list_products(name: str = Query(default=None, min_length=1, max_length=75, d
         reverse = order == "desc"    ## for deciding Order
         products = sorted(products,key = lambda p : p.get("price",0), reverse = reverse )
 
+    products = products[offset: offset+  limit]
     total = len(products)
 
-    return {"total" : total, "Items" : products}
+    return {"total" : total, "limit" : limit, "Items" : products}
 
 
