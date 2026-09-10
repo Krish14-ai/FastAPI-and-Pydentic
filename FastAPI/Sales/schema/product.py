4d87582f7c6f9a8c5e412c2de76eca220decfade
krish14-ai <kkhandelwal292@gmail.com>
2026-09-11 00:44:19 +0530

    done for today

--- FastAPI/Sales/schema/product.py
+++ FastAPI/Sales/schema/product.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator, computed_field, model_validator
 from typing import Annotated, Optional
 from typing import Dict
-
+from datetime import datetime
 
 class Product_class(BaseModel):
     id : int = 0
@@ -11,14 +11,16 @@ class Product_class(BaseModel):
                     max_length= 100, 
                     min_length= 0, 
                     title= "Product Name",
-                    examples=["shoes", "phones"]
-                    description= "Please Enter a product name"),
-                    
+                    examples=["shoes", "phones"],
+                    description= "Please Enter a product name"
+                    )
                     ]
     category = Annotated[str, Field(description= "Enter the category of product")]
     price = Annotated[float,Field(description= "Enter the price", ge = 1) ]
     stock = Annotated[int, Field(description= "This is the total stock left ")]
     rating = Annotated[float, Field(gt =0, le =5 , description="Rating of Product")]
     in_stock = Annotated[bool, Field(description="Tells if the product is in stock")]
-    seller = Annotated[Dict,Field(description="Details of seller")]
+    seller = Annotated[Dict, Field(description="Details of Seller")]
+    ## Seller
+    created_at = datetime
 
