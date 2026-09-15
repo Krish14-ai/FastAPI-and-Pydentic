1126c02fc60e543a9bdd0112dc8a9b76a6c62db5
krish14-ai <kkhandelwal292@gmail.com>
2026-09-15 23:12:43 +0530

    hi

--- FastAPI/Sales/schema/product.py
+++ FastAPI/Sales/schema/product.py
@@ -140,3 +140,26 @@ class Product_class(BaseModel):
         
         return self
 #=======================================================================================
+
+
+#=======================================================================================
+## Product Update Class
+#=======================================================================================
+class Product_update(BaseModel):
+        name : Annotated[
+                str, 
+                Field(
+                    max_length= 100, 
+                    min_length= 0, 
+                    title= "Product Name",
+                    examples=["shoes", "phones"],
+                    description= "Please Enter a product name"
+                    )
+                    ]
+    category : Annotated[str, Field(description= "Enter the category of product")]
+    price : Annotated[float,Field(description= "Enter the price", ge = 1) ]
+    stock : Annotated[int, Field(description= "This is the total stock left ")]
+    rating : Annotated[float, Field(gt =0, le =5 , description="Rating of Product")]
+    in_stock : Annotated[bool, Field(description="Tells if the product is in stock")]
+    seller : Seller
+    created_time : datetime
